ca016e05212cf80bff1404ca3c5d19e567543e3f
Adrian Hinds <adrianhinds@adrians-mbp.mynetworksettings.com>
2024-09-10 18:15:19 -0400

    latest commit

--- index.js
+++ index.js
@@ -73,7 +73,58 @@ async function getRandomImage() {
  *   by setting a default header with your API key so that you do not have to
  *   send it manually with all of your requests! You can also set a default base URL!
  */
-const response = axios.post()
+
+async function initialLoad() {
+  axios({
+      method: 'get',
+      onDownloadProgress: (progressEvent) => updateProgress(progressEvent),
+      url: `https://api.thecatapi.com/v1/breeds`,
+  })
+  .then(response => {
+      response.data.forEach(element => {
+          let newOption = document.createElement("option")
+          newOption.value = element.id;
+          newOption.innerText = element.name
+          breedSelect.appendChild(newOption)
+      });
+   })
+  displayInfo(`https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=abys&api_key=${API_KEY}`);
+}
+
+breedSelect.addEventListener('change', function() {
+  displayInfo(`https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=${breedSelect.value}&api_key=${API_KEY}`)
+})
+async function displayInfo(url) {  
+  try {
+      Carousel.clear();
+      infoDump.innerHTML=''
+
+await axios.get(url, {
+  headers: {
+      'x-api-key': API_KEY,
+      
+  },
+  onDownloadProgress: (progressEvent) => updateProgress(progressEvent),
+}).then(response => {
+  if (response.data.length > 0) {
+      makeCar(response.data)
+  } else {
+      infoDump.innerHTML = "<h1>No images found.</h1>";
+  }
+})
+Carousel.start()
+} catch (error) {
+console.error(error)
+}
+}
+
+function makeCar(array) {
+array.forEach(element => {
+let newImage = Carousel.createCarouselItem(element.url, "cat", element.id)
+Carousel.appendCarousel(newImage)
+}); 
+}
+
 /**
  * 5. Add axios interceptors to log the time between request and response to the console.
  * - Hint: you already have access to code that does this!
@@ -81,9 +132,30 @@ const response = axios.post()
  * - As an added challenge, try to do this on your own without referencing the lesson material.
  */
 axios.interceptors.request.use(request => {
-   console.log('Request sent.');
-   return request;
+  request.metadata = request.metadata || {};
+  request.metadata.startTime = new Date().getTime();
+  console.log("Request has begun.")
+  progressBar.style.width = "0%";
+  document.querySelector('body').style.cursor = "progress"
+  return request;
 });
+
+axios.interceptors.response.use(
+  (response) => {
+      response.config.metadata.endTime = new Date().getTime();
+      response.config.metadata.durationInMS = response.config.metadata.endTime - response.config.metadata.startTime;
+      console.log(`Request took ${response.config.metadata.durationInMS} milliseconds.`)
+      document.querySelector('body').style.cursor = "default"
+      return response;
+  },
+  (error) => {
+      error.config.metadata.endTime = new Date().getTime();
+      error.config.metadata.durationInMS = error.config.metadata.endTime - error.config.metadata.startTime;
+
+      console.log(`Request took ${error.config.metadata.durationInMS} milliseconds.`)
+      throw error;
+});
+
 /**
  * 6. Next, we'll create a progress bar to indicate the request is in progress.
  * - The progressBar element has already been created for you.
@@ -120,27 +192,75 @@ function updateProgress(ProgressEvent) {
  */
 export async function favourite(imgId) {
   // your code here
+
+try {
+  const favouritesResponse = await axios.get('https://api.thecatapi.com/v1/favourites', {
+    headers: {
+      'x-api-key': API_KEY,
+    },
+  });
+
+  const favourites = favouritesResponse.data
+  const existingFavourite = favourites.find(fav => fav.image_id === imgId);
+  // if liked 
+  if (existingFavourite) {
+    await axios.delete(`https://api.thecatapi.com/v1/favourites/${existingFavourite.id}`, {
+        headers: {
+          'x-api-key': API_KEY,
+        },
+      });
+  } else {
+    const response = await axios.post(
+        `https://api.thecatapi.com/v1/favourites`, 
+            {
+                image_id: imgId
+            }, 
+            {
+                headers: {
+                    'x-api-key': API_KEY,
+                },
+            }
+)
+console.log(response.data);
 }
+} catch (error) {
+console.error(error)
+}
+};
 
 /**
- * 9. Test your favourite() function by creating a getFavourites() function.
- * - Use Axios to get all of your favourites from the cat API.
- * - Clear the carousel and display your favourites when the button is clicked.
- *  - You will have to bind this event listener to getFavouritesBtn yourself.
- *  - Hint: you already have all of the logic built for building a carousel.
- *    If that isn't in its own function, maybe it should be so you don't have to
- *    repeat yourself in this section.
- */
-getFavourites() {
+* 9. Test your favourite() function by creating a getFavourites() function.
+* - Use Axios to get all of your favourites from the cat API.
+* - Clear the carousel and display your favourites when the button is clicked.
+*  - You will have to bind this event listener to getFavouritesBtn yourself.
+*  - Hint: you already have all of the logic built for building a carousel.
+*    If that isn't in its own function, maybe it should be so you don't have to
+*    repeat yourself in this section.
+*/
+export async function getFavourites() {
 
+try {
+Carousel.clear();
+infoDump.innerHTML=''
+const favouritesResponse = await axios.get('https://api.thecatapi.com/v1/favourites', {
+  headers: {
+    'x-api-key': API_KEY,
+  },
+})
+makeCar(favouriteImgs)
+Carousel.start()
+} catch (error) {
+console.error(error)
 }
-getFavourites.addEventListener("click")
-getFavouritesBtn {
 }
+
+getFavouritesBtn.addEventListener('click', getFavourites)
 /**
- * 10. Test your site, thoroughly!
- * - What happens when you try to load the Malayan breed?
- *  - If this is working, good job! If not, look for the reason why and fix it!
- * - Test other breeds as well. Not every breed has the same data available, so
- *   your code should account for this.
- */
+* 10. Test your site, thoroughly!
+* - What happens when you try to load the Malayan breed?
+*  - If this is working, good job! If not, look for the reason why and fix it!
+* - Test other breeds as well. Not every breed has the same data available, so
+*   your code should account for this.
+*/
+
+initialLoad();
